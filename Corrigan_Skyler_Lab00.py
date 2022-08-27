2cf420031fe373988a6148147331f3dd580909ca
ElfinPeach <83093493+ElfinPeach@users.noreply.github.com>
2022-08-27 15:43:33 -0700

    Add files via upload

---
+++ Corrigan_Skyler_Lab00.py
@@ -0,0 +1,10 @@
+#####################################################################
+#                                                                   #
+# Skyler Corrigan                                                   #
+# ECE 351-52                                                        #
+# Lab 00                                                            #
+# 09/01/2022                                                        #
+# GitHub Reports: https://github.com/ElfinPeach/ECE351_Report.git   #
+# GitHub Codes: https://github.com/ElfinPeach/ECE351_Code.git       #
+#                                                                   #
+#####################################################################
